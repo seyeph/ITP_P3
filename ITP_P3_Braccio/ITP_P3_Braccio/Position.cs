24f3b9bfce45c5cc47f1f640e64dabd672edc424
Philipp Ollmann <40146720140052@litec.ac.at>
2018-01-30 12:29:42 +0100

    added class ContorlList and Position

---
+++ ITP_P3_Braccio/ITP_P3_Braccio/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITP_P3_Braccio
+{
+    class Position
+    {
+        private int basicAngle;
+        
+    }
+}
