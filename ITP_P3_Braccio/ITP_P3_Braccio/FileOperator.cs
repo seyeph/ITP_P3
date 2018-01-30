338bf831dd672051b398d0cc3485c41d2a0f059e
Philipp Seyerl <40146720140061@litec.ac.at>
2018-01-30 12:48:30 +0100

    added FileOperator, not finished yet

---
+++ ITP_P3_Braccio/ITP_P3_Braccio/FileOperator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ITP_P3_Braccio
+{
+    class FileOperator
+    {
+        public bool writeFile(string path, string coords) {
+            bool operatingWell = true;
+
+            StreamWriter sw = new StreamWriter(path);
+
+            try
+            {
+                sw.Write(coords);
+            }
+            catch (Exception e)
+            {
+                operatingWell = false;
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                sw.Close();
+            }
+            return operatingWell;
+        }
+
+        public bool readFile(string path)
+        {
+            bool operatingWell = true;
+
+            //was i nu ned :D
+        }
+    }
+}
