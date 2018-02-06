3f3c8f902e390fcab97e73e603e0a7dab8d3225d
Michael Rath <40146720140057@litec.ac.at>
2018-02-06 12:39:27 +0100

    edited communication

--- ITP_P3_Braccio/ITP_P3_Braccio/Communication.cs
+++ ITP_P3_Braccio/ITP_P3_Braccio/Communication.cs
@@ -3,23 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
 
 namespace ITP_P3_Braccio
 {
     public static class Communication
     {
-        public static void Move(ControlList controlList)
+        public static void Move(string Port, ControlList controlList)
         {
-            foreach(Movement m in controlList)
+            SerialPort port = new SerialPort(Port);
+            try
             {
-                if(m is Position)
+                port.Open();
+                foreach (Movement m in controlList)
                 {
- 
-                }
-                else
-                {
+                    if (m is Position)
+                    {
+                        Position position = (Position)m;
 
+                    }
+                    else
+                    {
+                        if (m is Pause)
+                        {
+                            Task.Delay(((Pause)m).Pause_ms);
+                        }
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                port.Close();
             }
         }
     }
